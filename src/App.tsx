dcd7a43e55c800d242ef5b4dc04f9514712deb24
Zachary Traczyk <cookies951@gmail.com>
2022-06-21 11:05:49 -0500

    basic soccer api axios get and log

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [appState, setAppState] = useState({
+    loading: false,
+    videos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true, videos: null });
+    const options = {
+      method: 'GET',
+      url: 'https://free-football-soccer-videos.p.rapidapi.com/',
+      headers: {
+        'X-RapidAPI-Key': '80dded9258mshfd9acabb32702b9p19ca5ejsn65d1618aa741',
+        'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      const allVideos = response.data;
+      setAppState({ loading: false, videos: allVideos });
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [setAppState]);
+
+  console.log(appState.videos)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <h1>Soccer API</h1>
+    </>
   )
 }
 
